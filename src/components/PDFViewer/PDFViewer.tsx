18d3c2ee32d0b55e1690f7949aedd37249b96f95
nicharee.ka <n.kalvibool@gmail.com>
2024-02-02 09:20:13 +0700

    [UPDATE]: Update pdf render

--- src/components/PDFViewer/PDFViewer.tsx
+++ src/components/PDFViewer/PDFViewer.tsx
@@ -1,7 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import styles from "./PDFViewer.module.scss";
-import { isMobile } from "react-device-detect";
-import useRenderPDF from "../../utils/renderPDF";
+import { isMobile, isTablet } from "react-device-detect";
+import useRenderPDF, { ZOOM_SCALE } from "../../utils/renderPDF";
+import { ChevronLeft, ChevronRight, Download, Loader, MinusCircle, PlusCircle, ZoomIn, ZoomOut } from "react-feather";
+import classNames from "classnames";
 
 export interface PDFViewerProps {
     src: string;
@@ -9,39 +11,113 @@ export interface PDFViewerProps {
 const regex = /(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/;
 
 const PDFViewer = ({ src }: PDFViewerProps) => {
-    const [page_number, setPageNumber] = useState(1);
-    const { isLoading, errors, render } = useRenderPDF({
+    const { isLoading, totalPages, render, activePage, nextPage, previousPage, zoomIn, zoomOut, scale } = useRenderPDF({
         src,
         id: "pdf",
-        viewport: {
-            scale: 1,
-            dontFlip: true,
-            rotation: 0,
-        },
-        page_number: page_number,
     });
 
-    const handleRenderPDF = async () => {
-        await render();
+    // const handleRenderPDF = async () => {
+    //     await render();
+    // };
+
+    const handleReRenderPDF = async () => {
+        if (src && !regex.test(src)) {
+            await render();
+        }
     };
 
     useEffect(() => {
-        // if (src && !regex.test(src) && isMobile) {
-        handleRenderPDF();
-        // }
-    }, [src, page_number]);
+        handleReRenderPDF();
+    }, [activePage, scale]);
+
+    const handleNextPage = () => {
+        nextPage();
+    };
+
+    const handlePreviousPage = () => {
+        previousPage();
+    };
 
     return (
         <div className={styles.container}>
-            {/* {!isMobile ? (
+            {/* {!isMobile && !isTablet ? (
+            // {isMobile && isTablet ? (
                 <iframe
                     src={src}
                     title="PDF Viewer"
                 />
             ) : ( */}
             <div className={styles.canvasContainer}>
-                <div className={styles.canvasBox}>
+                <div className={styles.topBar}>
+                    <div className={styles.leftSection}></div>
+                    <div className={styles.certerSection}>
+                        <div
+                            className={classNames(styles.actionBtn, { [styles.disabled]: activePage <= 1 })}
+                            onClick={() => handlePreviousPage()}
+                        >
+                            <ChevronLeft
+                                size={16}
+                                className={styles.icon}
+                            />
+                        </div>
+                        <span className={classNames(styles.white, styles.pageInfo)}>
+                            {isLoading ? (
+                                "calculating"
+                            ) : (
+                                <>
+                                    {activePage} of {totalPages}
+                                </>
+                            )}
+                        </span>
+                        <div
+                            className={classNames(styles.actionBtn, { [styles.disabled]: totalPages <= activePage })}
+                            onClick={() => handleNextPage()}
+                        >
+                            <ChevronRight
+                                size={16}
+                                className={styles.icon}
+                            />
+                        </div>
+                    </div>
+                    <div className={styles.rightSection}>
+                        <div className={styles.zoom}>
+                            <div
+                                className={classNames(styles.actionBtn, { [styles.disabled]: scale <= 0 })}
+                                onClick={() => zoomOut()}
+                            >
+                                <ZoomOut
+                                    size={16}
+                                    className={styles.icon}
+                                />
+                            </div>
+                            <span className={classNames(styles.white, styles.scale)}>{ZOOM_SCALE[scale].text}</span>
+                            <div
+                                className={classNames(styles.actionBtn, { [styles.disabled]: scale >= 5 })}
+                                onClick={() => zoomIn()}
+                            >
+                                <ZoomIn
+                                    size={16}
+                                    className={styles.icon}
+                                />
+                            </div>
+                        </div>
+                        <div className={styles.divider}>|</div>
+                        <div className={styles.actionBtn}>
+                            <Download
+                                size={16}
+                                className={styles.icon}
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div
+                    id="pdf-container"
+                    className={styles.canvasBox}
+                >
                     <canvas id="pdf" />
+                    <div className={classNames(styles.canvasLoading, { [styles.show]: isLoading })}>
+                        <Loader size={26} />
+                    </div>
                 </div>
             </div>
             {/* )} */}
--- src/utils/renderPDF.ts
+++ src/utils/renderPDF.ts
@@ -1,67 +1,147 @@
 import * as PDFJS from "pdfjs-dist";
 // @ts-ignore
 import { WorkerMessageHandler } from "pdfjs-dist/build/pdf.worker.mjs";
-import { GetViewportParameters } from "pdfjs-dist/types/src/display/api";
+import { GetViewportParameters, RenderParameters } from "pdfjs-dist/types/src/display/api";
 import { useEffect, useMemo, useState } from "react";
 PDFJS.GlobalWorkerOptions.workerSrc = WorkerMessageHandler;
 
 type PDFRendererProps = {
     id: string;
     src: string | ArrayBuffer;
-    page_number: number;
-    viewport: GetViewportParameters;
+    defaultPage?: number;
+    viewport?: GetViewportParameters;
 };
 
 type PDFWorkingProps = {
     isLoading: boolean;
     errors: object;
+    totalPages: number;
+    activePage: number;
+    scale: number;
     render: () => Promise<void>;
+    onUpdateActivePage: (page: number) => void;
+    nextPage: () => void;
+    previousPage: () => void;
+    zoomIn: () => void;
+    zoomOut: () => void;
 };
 
-const useRenderPDF = (params: PDFRendererProps): PDFWorkingProps => {
-    const { src = "", page_number = 1, viewport, id } = params;
+const DefaultParams: GetViewportParameters = {
+    dontFlip: false,
+    rotation: 0,
+    scale: 5,
+};
 
+type ZoomScaleItemProps = { text: string; size: number };
+
+type ZoomScaleProps = {
+    [n: number]: ZoomScaleItemProps;
+};
+
+export const ZOOM_SCALE: ZoomScaleProps = {
+    [0]: { text: "50%", size: 0.5 },
+    [1]: { text: "75%", size: 0.75 },
+    [2]: { text: "90%", size: 0.9 },
+    [3]: { text: "100%", size: 1 },
+    [4]: { text: "125%", size: 1.25 },
+    [5]: { text: "150%", size: 1.5 },
+};
+
+const useRenderPDF = ({ id, src, defaultPage = 1, viewport = DefaultParams }: PDFRendererProps): PDFWorkingProps => {
     const [is_loading, setIsLoading] = useState<boolean>(false);
     const [errors, setErrors] = useState({});
+    const [total_pages, setTotalPages] = useState<number>(0);
+    const [scale, setScale] = useState<number>(3);
+    const [active_page, setActivePage] = useState(defaultPage);
+    // const [file_name, setFileName] = useState<string>("");
 
     const render: () => Promise<void> = (): Promise<void> => {
         return new Promise(async (resolve, rejecte) => {
             setIsLoading(true);
             try {
-                const loadingTask = PDFJS.getDocument(src);
-                const pdf = await loadingTask.promise;
+                const pdf = await PDFJS.getDocument(src).promise;
 
                 // Load information from the first page.
-                const page = await pdf.getPage(page_number);
-                const viewports = page.getViewport({ ...viewport, dontFlip: true, rotation: viewport.rotation || 0 });
+                const page = await pdf.getPage(active_page);
+                const viewports = page.getViewport(viewport);
 
                 // Apply page dimensions to the `<canvas>` element.
+                const unscaledViewport = page.getViewport({ scale: 1 });
+
                 const canvas = document.getElementById(id) as HTMLCanvasElement;
                 const context = canvas.getContext("2d");
-                canvas.height = viewports.height;
-                canvas.width = viewports.width;
+                if (context) {
+                    context.clearRect(0, 0, canvas.width, canvas.height);
+                    context.beginPath();
+                }
+                // context.scale(ZOOM_SCALE[scale].size, ZOOM_SCALE[scale].size);
+                // canvas.height = viewports.height;
+                // canvas.width = viewports.width;
+
+                canvas.height = viewports.height * ZOOM_SCALE[scale].size;
+                canvas.width = viewports.width * ZOOM_SCALE[scale].size;
+                canvas.style.height = `${unscaledViewport.height * ZOOM_SCALE[scale].size}px`;
+                canvas.style.width = `${unscaledViewport.width * ZOOM_SCALE[scale].size}px`;
 
                 // Render the page into the `<canvas>` element.
-                const renderContext = {
+                const renderContext: RenderParameters = {
                     canvasContext: context,
                     viewport: viewports,
+                    transform: [ZOOM_SCALE[scale].size, 0, 0, ZOOM_SCALE[scale].size, 0, 0],
                 };
                 page.render(renderContext);
+                setTotalPages(pdf.numPages);
                 setIsLoading(false);
                 resolve();
             } catch (error) {
-                // console.log(JSON.stringify(error));
                 setIsLoading(false);
                 setErrors(error);
+                // console.log(error);
                 rejecte();
             }
         });
     };
 
+    const handleChangePage = (page: number): void => {
+        setActivePage(page);
+    };
+
+    const handleNextPage = (): void => {
+        if (total_pages > active_page) {
+            handleChangePage(active_page + 1);
+        }
+    };
+
+    const handlePreviousPage = (): void => {
+        if (active_page > 1) {
+            handleChangePage(active_page - 1);
+        }
+    };
+
+    const handleZoomIn = (): void => {
+        if (scale < 5) {
+            setScale(scale + 1);
+        }
+    };
+
+    const handleZoomOut = (): void => {
+        if (scale > 0) {
+            setScale(scale - 1);
+        }
+    };
+
     return {
         isLoading: is_loading,
         errors: errors,
+        totalPages: total_pages,
+        activePage: active_page,
+        scale: scale,
         render: render,
+        onUpdateActivePage: handleChangePage,
+        nextPage: handleNextPage,
+        previousPage: handlePreviousPage,
+        zoomIn: handleZoomIn,
+        zoomOut: handleZoomOut,
     };
 };
 
